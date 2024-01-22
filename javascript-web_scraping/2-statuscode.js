c956d1b63855374edd94a6e5aee4aace48c94b76
Facusan2016 <facusan2016@gmail.com>
2024-01-22 19:30:15 -0300

    fix semistandrad styles

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -5,7 +5,7 @@ const request = require('request');
 const [, , URL] = process.argv;
 
 request.get(URL, (err, res, body) => {
-  if (err){
+  if (err) {
     console.error(err);
   }
   console.log(`code: ${res.statusCode}`);
